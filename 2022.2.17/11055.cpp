99e373fe5ae9fb000e46508db9c463d573b2ac3d
gokimkq123 <gokimkq111@naver.com>
2022-02-18 00:29:46 +0900

    2022.2.17

---
+++ 2022.2.17/11055.cpp
@@ -0,0 +1,39 @@
+//난이도 : 실버 2, 유형 : dp
+
+#include <iostream>
+#include <vector>
+#include <memory.h>
+#include <queue>
+#include <tuple>
+#include <algorithm>
+#include <unordered_map>
+#include <stack>
+#include <deque>
+#define l long long
+#define INF 2e9
+#define p pair<int,int>
+using namespace std;
+
+int dp[1001],num[1001];
+
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+    int n,ans = 0;cin >> n;
+
+    for(int i=0;i<n;i++){
+        cin >> num[i];
+        dp[i] = num[i];
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            if(num[i] > num[j]) dp[i] = max(dp[i],dp[j] + num[i]);
+        }
+    }
+
+    for(int i=0;i<n;i++)
+        ans = max(ans,dp[i]);
+
+    cout << ans;
+}
+
