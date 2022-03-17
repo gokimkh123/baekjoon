8a2909e0404a529ba037bcb8c2bb2f6a320287af
gokimkq123 <gokimkq111@naver.com>
2022-03-17 16:04:08 +0900

    2022.3.17

---
+++ 2022.3.17/12738.cpp
@@ -0,0 +1,32 @@
+//난이도 : 골드2, 유형 : 이분탐색
+
+#include <bits/stdc++.h>
+#define l long long
+#define INF 2e9
+#define p pair<int,int>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+
+
+    int n,ans = 0,temp;
+    cin >> n;
+    vector<int> v(n,-1000000001);
+
+    for(int i=0;i<n;i++){
+        cin >> temp;
+
+        if(temp > v.back()){
+            v.push_back(temp);
+            ans++;
+        }
+        else{
+            auto idx = lower_bound(v.begin(),v.end(),temp);
+            *idx = temp;
+        }
+    }
+
+    cout << ans;
+}
+
