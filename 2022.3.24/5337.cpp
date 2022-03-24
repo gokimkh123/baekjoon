0475b44fc1ad9980206ff441cd0fa5dd9c3457b1
gokimkq123 <gokimkq111@naver.com>
2022-03-24 14:51:22 +0900

    2022.3.24

---
+++ 2022.3.24/5337.cpp
@@ -0,0 +1,14 @@
+//난이도 : 브론즈5, 유형 : 입출력
+
+#include <bits/stdc++.h>
+#define l long long
+#define INF 2e9
+#define p pair<int,int>
+#define vc vector<l>
+using namespace std;
+
+int main(){
+   ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+
+    printf(".  .   .\n|  | _ | _. _ ._ _  _\n|/\\|(/.|(_.(_)[ | )(/.\n");
+}
