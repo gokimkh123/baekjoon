be289794f997841fa8f253c75ba6665605f10679
gokimkq123 <gokimkq111@naver.com>
2022-06-04 23:37:10 +0900

    2022.6.4

---
+++ 2022.6.4/1715.cpp
@@ -0,0 +1,50 @@
+//난이도 : 골드4, 유형 : 우선순위 큐, 그리디
+
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+#define INF 2e9
+#define F first
+#define S second
+using namespace std;
+
+typedef long long l;
+typedef pair<int,int> p;
+typedef tuple<int,int,int> tu;
+typedef vector<int> vc;
+
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+    priority_queue<int,vc,greater<>> q;
+
+    int n,a,ans = 0,b;
+
+    cin >> n;
+
+    for(int i=0;i<n;i++){
+        cin >> a;
+
+        q.push(a);
+    }
+
+    if(n == 1){
+        cout << 0;
+        return 0;
+    }
+
+    while(1){
+
+        a = q.top();q.pop();
+        b = q.top();q.pop();
+        ans += a + b;
+
+        if(q.empty()){
+            cout << ans;
+            return 0;
+        }
+
+        q.push(a+b);
+
+    }
+}
+
