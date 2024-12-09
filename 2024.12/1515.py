d0fb264b45a67dec5763c7604bfb67afff4325ac
gokimkq123 <gokimkq111@naver.com>
2024-12-09 17:11:29 +0900

    solved 12.09

---
+++ 2024.12/1515.py
@@ -0,0 +1,16 @@
+import math
+import sys
+from collections import Counter
+import itertools
+
+N = input()
+n = 0
+idx = 0
+while True:
+    n += 1
+    for s in str(n):
+        if N[idx] == s:
+            idx += 1
+            if idx >= len(N):
+                print(n)
+                exit()
